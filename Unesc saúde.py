75b28d96b7218dc95b7f533500dc3f5ab76e0422
Luiz <luizclasher42@gmail.com>
2024-07-30 20:56:23 -0300

    oi

---
+++ Unesc saúde.py
@@ -0,0 +1,4 @@
+nome_do_paciente = (input("Escreva seu nome: "))
+alt_paciente = float (input("Digite sua altura: "))
+peso_paciente =  float (input("Digite seu peso: "))
+print(f"Bem vindo ao Unesc saúde", f"{nome_do_paciente}!", f"sua altura é: {alt_paciente}",f"e seu peso é: {peso_paciente}")
