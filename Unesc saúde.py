3a8be480513e3f73f8af203531f86a9775673556
Luiz <luizclasher42@gmail.com>
2024-07-30 21:48:29 -0300

    j

--- Unesc saúde.py
+++ Unesc saúde.py
@@ -1,5 +1,18 @@
-nome_do_paciente = (input("Escreva seu nome: "))
-alt_paciente = float (input("Digite sua altura: "))
-peso_paciente =  float (input("Digite seu peso: "))
-imc = peso_paciente / (alt_paciente **2)
-print(f"Bem vindo ao Unesc saúde", f"{nome_do_paciente}!", f"seu IMC é: {imc:.2f}")
+print("Bem vindo ao UNESC saúde")
+print ("""MENU
+       1 - Calcular IMC
+       2 - Calcular Taxa metabólica Basal
+       3 - Sair """)
+opcao = int  (input("Digite a opção desejada: "))
+
+if opcao == 1:
+    alt_paciente = float (input("Digite sua altura: "))
+    peso_paciente =  float (input("Digite seu peso: "))
+    imc = peso_paciente / (alt_paciente **2)
+    print(f"Seu IMC é: {imc:.2f}")
+elif opcao == 2:
+    print("Taxa metabólica")
+elif opcao == 3:
+    print("Obrigado por usar o UNESC saúde!")
+else:
+    print("Erro")
