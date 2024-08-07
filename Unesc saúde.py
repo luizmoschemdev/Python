60f4608ae4d33a8d0620eb35a5506378f8322bd1
Luiz <luizclasher42@gmail.com>
2024-08-06 21:54:30 -0300

    ss

--- Unesc saúde.py
+++ Unesc saúde.py
@@ -5,7 +5,10 @@ idade = int (input("Digite sua idade: "))
 alt_paciente = float (input("Digite sua altura: "))
 peso_paciente =  float (input("Digite seu peso: "))
 
-print ("""MENU
+voltar_ao_menu = True
+while voltar_ao_menu == True:
+
+    print ("""MENU
        1 - Calcular IMC
        2 - Calcular Taxa metabólica Basal
        3 - Sair """)
@@ -17,13 +20,14 @@ if opcao == 1:
 elif opcao == 2:
     alt_em_cm = alt_paciente * 100
     match sexo:
-        case "M" | "m":
+        case "M" | "m" | "Masculino" | "Homem" | "Macho":
             tmb = 66 + (13.7 * peso_paciente) + (5 * alt_em_cm) - (6.8 * idade)
             print(f"Sua taxa metabólica basal é {tmb} calorias")
-        case "F" | "f": 
+        case "F" | "f" | "Feminino" | "Mulher" | "Moça": 
             tmb = 665 + (9.6 * peso_paciente) + (1.8 * alt_em_cm) - (4.7 * idade)
             print(f"Sua taxa metabólica é: {tmb} calorias")
 elif opcao == 3:
     print("Obrigado por usar o UNESC saúde!")
-else:
+    voltar_ao_menu = False 
+else: 
     print("Opção inválida")
