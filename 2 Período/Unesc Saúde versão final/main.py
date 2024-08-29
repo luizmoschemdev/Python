304a636d11b3fdeeccfe1277e6020c09d205c881
Luiz <luizclasher42@gmail.com>
2024-08-29 10:26:31 -0300

    lkkkk

--- 2 Período/Unesc Saúde versão final/main.py
+++ 2 Período/Unesc Saúde versão final/main.py
@@ -3,7 +3,8 @@ from menu import boas_vindas, mostrar_menu, coletar_dados
 
 def main():
     boas_vindas()
-    dados_paciente = coletar_dados()
-    mostrar_menu()
+    dados_pacientes = coletar_dados()
+    mostrar_menu(*dados_pacientes)
+
 if __name__ == "__main__":
     main()
