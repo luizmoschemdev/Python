ac1b8858c59f6f51b72e41dc5546328af8bdfa81
Luiz <luizclasher42@gmail.com>
2024-08-29 10:14:45 -0300

    ranquei fora o negocio

--- 2 Período/Unesc Saúde versão final/main.py
+++ 2 Período/Unesc Saúde versão final/main.py
@@ -4,6 +4,6 @@ from menu import boas_vindas, mostrar_menu, coletar_dados
 def main():
     boas_vindas()
     dados_paciente = coletar_dados()
-    mostrar_menu(dados_paciente)
+    mostrar_menu()
 if __name__ == "__main__":
     main()
