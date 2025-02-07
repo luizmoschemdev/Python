cb6aa8e5b24cdc73e30b236d29fc05737405f37d
Luiz <luizclasher42@gmail.com>
2025-02-07 19:41:10 -0300

    gyuyigouiuihui

--- 2 Período/flask/venv/main.py
+++ 2 Período/flask/venv/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('biblio.html')
+    return render_template('biblio.html', title = 'biblioteca unesc')
 
 if __name__ == "__main__":
     app.run(debug=True)
